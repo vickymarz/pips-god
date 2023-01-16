02cc077dd3d829add281a5f72d0ad5456221797f
vickymarz <okorojiebube2@gmail.com>
2023-01-16 14:38:26 +0100

    Remove img

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -4,9 +4,8 @@ import logo from '../../assets/images/logo.png'
 export const Logo = () => {
 	return (
 		<Link to='/'>
-			<div className="flex justify-start items-center gap-x-[10px]">
+			<div className="flex justify-start items-start">
               <img src={logo} alt="logo" className="w-[27px] h-[23px] md:w-[50px] md:h-[40px]"/>
-              <h1 className="text-white text-[14px] md:text-[1.2rem] font-bold">Pips god</h1>
 			</div>
 		</Link>
 	);
