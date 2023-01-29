121c2f93055cef15aec1bcdaa6796c1fe238f659
vickymarz <okorojiebube2@gmail.com>
2023-01-29 20:46:36 +0100

    Style analytics page

--- src/pages/admin/homepage/components/overview/components/analytics/index.tsx
+++ src/pages/admin/homepage/components/overview/components/analytics/index.tsx
@@ -2,9 +2,9 @@ import { AnalyticsCard, AnalyticsGraph } from "./components"
 
 export const Analytics = () => {
   return (
-    <div className="flex flex-col justify-start items-start gap-y-[32x]">
-      <div className="flex justify-between items-center ">
-        <div className='flex flex-col justify-start items-center gap-y-[10px]'>
+    <div className="flex flex-col justify-start items-start gap-y-[32px] mt-[16px]">
+      <div className="flex justify-between items-center w-full">
+        <div className='flex flex-col justify-start items-start gap-y-[10px]'>
           <h2 className='text-[#19275E] font-semibold'>
             Analytics
           </h2>
@@ -12,7 +12,7 @@ export const Analytics = () => {
             See your website statistics
           </p>
         </div>
-        <div className="border-[8px] bg-[#F5F9FA] py-[14px] px-[16px] text-[#19275E] font-semibold">
+        <div className="rounded-[8px] border-0 outline-0 bg-[#F5F9FA] py-[14px] px-[16px] text-[#19275E] font-semibold">
           Dec 21st 2022- Jan 14th 2023
         </div>
       </div>
--- src/pages/admin/homepage/index.tsx
+++ src/pages/admin/homepage/index.tsx
@@ -9,14 +9,14 @@ export const Admin = () => {
     <div className='px-6 py-[24px] md:px-[80px]'>
       <Logo />
       <AdminHeader />
-      <ul className="flex justify-start items-center gap-x-[45px] border border-[#D3D3D3]">
+      <ul className="flex justify-start items-center gap-x-[45px] border-b border-[#D3D3D3] mt-[20px]">
         <li>
             <Button type='button'
                 className={`${
                   status === "overview"
-                    ? "px-[16px] py-[14px] bg-[#F5F9FA] border-[3px] border-[#0D142E] font-semibold text-[#19275E] text-[1.37rem]"
-                    : ""
-                }'outline-0 border-0 text-[#53717A] bg-inherit text-[1.37rem] font-medium'`}
+                    ? "px-[16px] py-[14px] bg-[#F5F9FA] border-b-[3px] border-[#0D142E] font-semibold text-[#19275E]"
+                    : "text-[#53717A]"
+                }'outline-0 focus:outline-0 border-0 text-[#53717A] text-[1.37rem] font-medium'`}
                 onClick={() => setStatus("overview")}
               >
                 Overview
@@ -27,9 +27,9 @@ export const Admin = () => {
                 type="button"
                 className={`${
                   status === "courses"
-                    ? "px-[16px] py-[14px] bg-[#F5F9FA] border-[3px] border-[#0D142E] font-semibold text-[#19275E] text-[1.37rem]"
-                    : ""
-                }' outline-0 border-0 text-[#53717A] bg-inherit text-[1.37rem] font-medium'`}
+                    ? "px-[16px] py-[14px] bg-[#F5F9FA] border-b-[3px] border-[#0D142E] font-semibold text-[#19275E]"
+                    : "text-[#53717A]"
+                }'outline-0 border-0 focus:outline-0 text-[#53717A] text-[1.37rem] font-medium'`}
                 onClick={() => setStatus("courses")}
               >
                Courses
