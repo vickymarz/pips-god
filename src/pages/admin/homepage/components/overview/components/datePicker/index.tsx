8837d1613991adeb60dc4b944480a2c7404df463
vickymarz <okorojiebube2@gmail.com>
2023-01-30 23:44:37 +0100

    Style course form modal

--- src/pages/admin/homepage/components/overview/components/datePicker/index.tsx
+++ src/pages/admin/homepage/components/overview/components/datePicker/index.tsx
@@ -4,8 +4,8 @@ import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 import {Button} from 'components'
 
 export const DatePicker = ({setModal, modal}:{setModal: React.Dispatch<React.SetStateAction<boolean>>, modal: boolean}) => {
-    const [value, onChange] = useState([new Date(), new Date()]);
-    
+    const [value, onChange] = useState<React.SetStateAction<string | Date[]>>([new Date(), new Date()]);
+
     return (
     <div className={`${modal ?'fixed top-0 right-0 left-0 bottom-0 w-screen z-20 bg-[#69686844]': 'hidden'}`}>
       <div className='w-[26%] ml-auto mr-auto relative mt-44 flex flex-col justify-center items-center bg-white py-6 rounded-2xl h-min-[400px]'>
