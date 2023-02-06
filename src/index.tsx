12b0277c5a12ece254a3971e859615eeefc89a69
vickymarz <okorojiebube2@gmail.com>
2023-02-06 07:58:48 +0100

    Setup react query

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { QueryClientProvider, QueryClient } from 'react-query'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
