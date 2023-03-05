80c5a31cf4f5ec656839d0aa4b4585ae640b629c
vickymarz <okorojiebube2@gmail.com>
2023-03-05 02:34:52 +0100

    Modify registration route

--- src/components/paystackCallback/index.tsx
+++ src/components/paystackCallback/index.tsx
@@ -31,7 +31,7 @@ const { isError } = useTransactions(id, onSuccess)
         <div className='flex justify-center items-center'>
           <img src={success} alt="success" />
         </div>
-        <Link to="/register/:id"className="block font-productSans text-[1.06rem] font-bold mt-5 text-white bg-[#0D142E] rounded-[4px] py-[0.75rem] px-[3.8rem] tracking-[0.02em]">
+        <Link to={`/register/${id}`} className="block font-productSans text-[1.06rem] font-bold mt-5 text-white bg-[#0D142E] rounded-[4px] py-[0.75rem] px-[3.8rem] tracking-[0.02em]">
           Continue Registration
         </Link>
     </div>
--- src/components/verifyPopUp/index.tsx
+++ src/components/verifyPopUp/index.tsx
@@ -1,8 +1,8 @@
 
 export const VerifyPopup = ({verify}: {verify: boolean}) => {
     return (
-      <div className={`${verify ? 'fixed top-0 right-0 left-0 bottom-0 min-h-screen w-screen w-screen z-20 bg-[#69686844] overflow-y-scroll' : 'hidden'}`}>
-        <div className='w-[80%] md:w-[50%] ml-auto mr-auto relative my-44 rounded-[27px]  bg-white p-[20px]'>
+      <div className={`${verify ? 'fixed top-0 right-0 left-0 bottom-0 min-h-screen w-screen w-screen z-20 bg-[#0D142e] overflow-y-scroll' : 'hidden'}`}>
+        <div className='w-[80%] md:w-[50%] ml-auto mr-auto relative my-44 rounded-[27px]  bg-white p-[30px] md:p-[40px]'>
           <h2 className=' mb-[20px] text-[#0D142E] font-bold text-[2rem] text-center'>Transaction not found</h2>
            <p>Sorry, your transaction cannot be found. Please ensure that you make payment before you proceed. Thanks!</p>
         </div>
