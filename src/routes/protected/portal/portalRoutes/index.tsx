dc3301410ced7c37fe983edbf745d77137d53689
vickymarz <okorojiebube2@gmail.com>
2023-03-07 20:36:38 +0100

    Add protected routes

--- src/routes/protected/portal/portalRoutes/index.tsx
+++ src/routes/protected/portal/portalRoutes/index.tsx
@@ -1,10 +1,15 @@
 
 import { Route, Routes } from 'react-router';
+import { ProtectedRoute } from 'utils';
 import {portalRoutesData} from '../portalRoutesData';
 
 export const PortalRoutes = () => {
     const portalRoutes = portalRoutesData.map(({ path, id, element }) => {
-        return <Route key={id} path={`/${path}`} element={element} />;
+        return <Route key={id} path={`/${path}`} element={
+          <ProtectedRoute>
+           { element }
+          </ProtectedRoute>
+        } />;
     });
 
     return <Routes>{portalRoutes}</Routes>;
--- src/routes/public/publicRoutes/index.tsx
+++ src/routes/public/publicRoutes/index.tsx
@@ -1,14 +1,9 @@
 import { Route, Routes } from 'react-router';
-import { ProtectedRoute } from 'utils';
 import publicRoutesData from '../publicRoutesData';
 
 export const PublicRoutes = () => {
     const publicRoutes = publicRoutesData.map(({ path, id, element }) => {
-        return <Route key={id} path={`/${path}`} element={
-        <ProtectedRoute>
-            { element }
-        </ProtectedRoute>
-        } />;
+        return <Route key={id} path={`/${path}`} element={element} />;
     });
 
     return <Routes>{publicRoutes}</Routes>;
