b807ab601f7c7232cbeb62902dec85a8833a581c
vickymarz <okorojiebube2@gmail.com>
2023-03-14 07:56:20 +0100

    Modify vip payment payload and response

--- src/components/paystackCallback/index.tsx
+++ src/components/paystackCallback/index.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import {  useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useTransactions } from 'hooks'
+import { useSearchParams } from "react-router-dom";
 import success from '../../assets/images/success.png'
 import { VerifyPopup } from "../verifyPopUp";
 
 export const PaystackCallback = () => {
   const [verify, setVerify] = useState(false)
-
+  const [vipSignal, setVipSignal] = useState<string | null>('')
   const { id } = useParams()
+  const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const vip_signal = searchParams?.get('vip_signal');
+    setVipSignal(vip_signal)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const onSuccess = (data:any) => {
     if (data.code === 404 || isError) {
@@ -23,18 +31,31 @@ const { isError } = useTransactions(id, onSuccess)
 
   return (
     <>
-    <div className='flex justify-center flex-col px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] w-full min-h-full'>
-        <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
-          <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
-          <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can proceed to continue your registration </p>
-        </div>
-        <div className='flex justify-center items-center'>
-          <img src={success} alt="success" />
-        </div>
-        <Link to={`/register/${id}`} className="block font-productSans text-[1.06rem] font-bold mt-5 text-white bg-[#0D142E] rounded-[4px] py-[0.75rem] px-[3.8rem] tracking-[0.02em]">
-          Continue Registration
-        </Link>
-    </div>
+    {vipSignal ?
+    (
+      <div className='flex justify-center flex-col px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] w-full min-h-full'>
+      <div className='flex justify-center items-center'>
+        <img src={success} alt="success" />
+      </div>
+      <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
+        <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
+        <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can now proceed to your email to join the vip telegram group via a link that will be sent to you. </p>
+      </div>
+  </div>
+    ): (
+      <div className='flex justify-center flex-col px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] w-full min-h-full'>
+      <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
+        <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
+        <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can proceed to continue your registration </p>
+      </div>
+      <div className='flex justify-center items-center'>
+        <img src={success} alt="success" />
+      </div>
+      <Link to={`/register/${id}`} className="block font-productSans text-[1.06rem] font-bold mt-5 text-white bg-[#0D142E] rounded-[4px] py-[0.75rem] px-[3.8rem] tracking-[0.02em]">
+        Continue Registration
+      </Link>
+  </div>
+    )}
       {verify && <VerifyPopup verify={verify} />}
     </>
   )
--- src/pages/external/vipPayment/components/vipPaymentForm/index.tsx
+++ src/pages/external/vipPayment/components/vipPaymentForm/index.tsx
@@ -4,7 +4,10 @@ import userServices from "services/userServices";
 import card from '../../../../../assets/images/card.png'
 
 type FormValues = {
+    firstName: string;
+    lastName: string;
     email: string;
+    telegramUsername: string;
 };
 
 export const VipPaymentForm = ({id}:{id:string}) => {
@@ -56,42 +59,161 @@ export const VipPaymentForm = ({id}:{id:string}) => {
 		// eslint-disable-next-line no-useless-escape
 		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    const thirdPattern = /^[a-z][a-z0-9\s]+$/gi;
+
   return (
     <>
         {errorMsg()}
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-start items-center w-full gap-y-[1rem] mt-[20px]' id={id}>
-            <div className='relative flex flex-col justify-start items-start w-full gap-y-[8px]'>
-                <label className='pb-0 text-[#19275E] md:text-[1.25rem] text-[1rem] ' htmlFor='email'>
-                    Email
+          <div className="w-full flex gap-y-[1rem] md:gap-y-[2.12rem] md:gap-x-[2.12rem] flex-col md:flex-row justify-start items-start">
+            <div className='relative flex flex-col justify-start items-start gap-y-[8px] w-full'>
+              <label className='pb-0 md:text-[#888888] text-[#666666] md:text-[1.25rem] text-[1rem] font-medium' htmlFor='firstName'>
+                First Name
+              </label>
+              <input
+                style={{
+                    borderBottom: errors.firstName && "2px solid red",
+                }}
+                className={`focus:outline-none focus:${
+                    !errors.email
+                        ? "shadow-[0px_0px_0px_4px_rgba(74,74,104,0.1)]"
+                        : "shadow-[0px_0px_0px_4px_rgba(249,50,50,0.1)]"
+                }  text-[#666666] w-full text-[0.75rem] md:text-[1rem] pr-[80px] py-[12px] px-[10px] rounded-[10px] bg-transparent border border-[#000000]`}
+                type='text'
+                id="firstName"
+                placeholder='Enter your first name'
+                {...register("firstName", {
+                    required: "First name cannot be empty",
+                    minLength: {
+                        value: 3,
+                        message: "First name must be at least 3 characters",
+                    },
+                    maxLength: {
+                        value: 30,
+                        message: "First name must not be more than 30 characters",
+                    },
+
+                    pattern: {
+                        value: thirdPattern,
+                        message:
+                            "First name must start with a letter and no special characters are allowed",
+                    },
+                })}
+            />
+            {errors.firstName && (
+                <p
+                    className='right-0 bottom-[-37px] italic text-sm mt-2'
+                    style={{ color: "red" }}>
+                    {errors.firstName?.message}
+                </p>
+            )}
+          </div>
+          <div className='relative flex flex-col justify-start items-start gap-y-[8px] w-full'>
+            <label className='pb-0 md:text-[#888888] text-[#666666] md:text-[1.25rem] text-[1rem] font-medium' htmlFor='lastName'>
+              Last Name
+            </label>
+            <input
+                style={{
+                    borderBottom: errors.lastName && "2px solid red",
+                }}
+                className={`focus:outline-none focus:${
+                    !errors.email
+                        ? "shadow-[0px_0px_0px_4px_rgba(74,74,104,0.1)]"
+                        : "shadow-[0px_0px_0px_4px_rgba(249,50,50,0.1)]"
+                }  text-[#666666] w-full text-[0.75rem] md:text-[1rem] pr-[80px] py-[12px] px-[10px] rounded-[10px] bg-transparent border border-[#000000]`}
+                type='text'
+                id="lastName"
+                placeholder='Enter your last name'
+                {...register("lastName", {
+                    required: "last name cannot be empty",
+                    minLength: {
+                        value: 3,
+                        message: "Last name must be at least 3 characters",
+                    },
+                    maxLength: {
+                        value: 30,
+                        message: "Last name must not be more than 30 characters",
+                    },
+
+                    pattern: {
+                        value: thirdPattern,
+                        message:
+                            "Last name must start with a letter and no special characters are allowed",
+                    },
+                })}
+            />
+            {errors.lastName && (
+                <p
+                    className='right-0 bottom-[-37px] italic text-sm mt-2'
+                    style={{ color: "red" }}>
+                    {errors.lastName?.message}
+                </p>
+            )}
+        </div>
+    </div>
+        <div className='relative flex flex-col justify-start items-start w-full gap-y-[8px]'>
+            <label className='pb-0 text-[#19275E] md:text-[1.25rem] text-[1rem] ' htmlFor='email'>
+                Email
+            </label>
+            <input
+                style={{
+                    border: errors.email && "1px solid red"
+                }}
+                className={`focus:outline-none focus:${
+                    !errors.email
+                        ? "shadow-[0px_0px_0px_4px_rgba(74,74,104,0.1)]"
+                        : "shadow-[0px_0px_0px_4px_rgba(249,50,50,0.1)]"
+                }  text-[#666666] w-full text-[0.75rem] md:text-[1rem] pr-[80px] py-[12px] px-[10px] rounded-[10px] bg-transparent border border-[#000000]`}
+                type='email'
+                id="email"
+                placeholder='Enter your active email address'
+                {...register("email", {
+                    required: "Email cannot be empty",
+                    pattern: {
+                        value: pattern,
+                        message: "Please enter a valid email",
+                    },
+                })}
+            />
+            {errors.email && (
+                <p className='italic text-sm mt-2' style={{ color: "red" }}>
+                    {errors.email?.message}
+                </p>
+            )}
+            </div>
+            <div className='relative flex flex-col justify-start items-start gap-y-[8px] w-full'>
+                <label className='pb-0 md:text-[#888888] text-[#666666] md:text-[1.25rem] text-[1rem] font-medium' htmlFor='telegramUsername'>
+                    Telegram Username
                 </label>
                 <input
                     style={{
-                        border: errors.email && "1px solid red"
+                        borderBottom: errors.telegramUsername && "2px solid red"
                     }}
                     className={`focus:outline-none focus:${
                         !errors.email
                             ? "shadow-[0px_0px_0px_4px_rgba(74,74,104,0.1)]"
                             : "shadow-[0px_0px_0px_4px_rgba(249,50,50,0.1)]"
                     }  text-[#666666] w-full text-[0.75rem] md:text-[1rem] pr-[80px] py-[12px] px-[10px] rounded-[10px] bg-transparent border border-[#000000]`}
-                    type='email'
-                    id="email"
-                    placeholder='Enter your email'
-                    {...register("email", {
-                        required: "Email cannot be empty",
-                        pattern: {
-                            value: pattern,
-                            message: "Please enter a valid email",
+                    id="telegramUsername"
+                    placeholder='Enter your telegram username'
+                    {...register("telegramUsername", {
+                        required: "Telegram username cannot be empty",
+                        minLength: {
+                            value: 3,
+                            message: "Telegram username must be at least 3 characters",
                         },
                     })}
                 />
-                {errors.email && (
-                    <p className='italic text-sm mt-2' style={{ color: "red" }}>
-                        {errors.email?.message}
+                {errors.telegramUsername && (
+                    <p
+                        className='right-0 bottom-[-37px] italic text-sm mt-2'
+                        style={{ color: "red" }}>
+                        {errors.telegramUsername?.message}
                     </p>
                 )}
             </div>
         </form>
-        <div className="hidden md:flex justify-center items-center w-full0 mt-[105px] md:mt-[80px]">
+        <div className="hidden md:flex justify-center items-center w-full mt-[105px] md:mt-[80px]">
             <img src={card} alt="" className="md:object-scale-down"/>
         </div>
     </>
