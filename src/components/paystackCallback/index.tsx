ad78892063456b0ab69ca42d0d1db06d94221af1
vickymarz <okorojiebube2@gmail.com>
2023-03-14 09:05:58 +0100

    Modify callback paystack UI

--- src/components/paystackCallback/index.tsx
+++ src/components/paystackCallback/index.tsx
@@ -29,37 +29,43 @@ export const PaystackCallback = () => {
 const { isError } = useTransactions(id, onSuccess)
 
 const vip = (
-  <div className='flex justify-center flex-col px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] w-full min-h-full'>
-  <div className='flex justify-center items-center'>
-    <img src={success} alt="success" />
-  </div>
-  <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
-    <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
-    <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can now proceed to your email to join the vip telegram group via a link that will be sent to you. </p>
+  <div className='bg-authImg bg-cover bg-no-repeat w-full min-h-screen'>
+    <div className="w-full min-h-screen px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] flex justify-center flex-col bg-[#ffffffe6] md:bg-[#F8FAFC]">
+      <div className='flex justify-center items-center'>
+        <img src={success} alt="success" />
+      </div>
+      <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
+        <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
+        <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can now proceed to your email to join the vip telegram group via a link that will be sent to you. </p>
+      </div>
   </div>
 </div>
 )
 
 const mentorship = (
-    <div className='flex justify-center flex-col px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] w-full min-h-full'>
-    <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
-      <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
-      <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can proceed to continue your registration </p>
+  <div className='bg-authImg bg-cover bg-no-repeat w-full min-h-screen'>
+    <div className="w-full min-h-screen px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] flex justify-center flex-col bg-[#ffffffe6] md:bg-[#F8FAFC]">
+      <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
+        <h2 className='text-[1.37rem] md:text-[2.5rem] font-bold'>Payment Successful.</h2>
+        <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Payment has been processed successfully, you can proceed to continue your registration </p>
+      </div>
+      <div className='flex justify-center items-center'>
+        <img src={success} alt="success" />
+      </div>
+      <Link to={`/register/${id}`} className="block font-productSans text-[1.06rem] font-bold mt-5 text-white bg-[#0D142E] rounded-[4px] py-[0.75rem] px-[3.8rem] tracking-[0.02em]">
+        Continue Registration
+      </Link>
     </div>
-    <div className='flex justify-center items-center'>
-      <img src={success} alt="success" />
-    </div>
-    <Link to={`/register/${id}`} className="block font-productSans text-[1.06rem] font-bold mt-5 text-white bg-[#0D142E] rounded-[4px] py-[0.75rem] px-[3.8rem] tracking-[0.02em]">
-      Continue Registration
-    </Link>
 </div>
 )
 
 const pending = (
-    <div className='flex justify-center flex-col px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] w-full min-h-full'>
+  <div className='bg-authImg bg-cover bg-no-repeat w-full min-h-screen'>
+  <div className="w-full min-h-screen px-[20px] py-[80px] md:px-[80px] items-center gap-y-[50px] flex justify-center flex-col bg-[#ffffffe6] md:bg-[#F8FAFC]">
     <div className='text-[#0D142E] flex justify-center flex-col gap-y-[20px] items-center md:font-semiBold'>
       <p className='text-center text-[0.75rem] md:text-[0.93rem]'>Please wait...</p>
     </div>
+  </div>
 </div>
 )
 
