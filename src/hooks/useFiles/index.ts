a1d2ecd8fc9503b487514ed3c362cfaac4b71d03
vickymarz <okorojiebube2@gmail.com>
2023-04-05 15:59:23 +0100

    Fix deploy errors

--- src/hooks/useFiles/index.ts
+++
@@ -1,7 +0,0 @@
-import {useMutation} from 'react-query'
-
-export const useFiles = ( queryFn: (params?: any) => Promise<any>, onSuccess:(data:string) => void ) => {
- return useMutation(queryFn, {
-   onSuccess: onSuccess
- })
-}
