bfa8c9907f1aa88802b74d527747e44b63022580
Victor Ebube Okoroji <okorojiebube2@gmail.com>
2023-04-06 15:41:01 +0100

    Update index.tsx

--- src/pages/external/emailVerificationSuccess/index.tsx
+++ src/pages/external/emailVerificationSuccess/index.tsx
@@ -12,7 +12,9 @@ const [searchParams] = useSearchParams();
 
 const {mutate, data} = useMutation(userServices.verifyEmail, {
   onSuccess: (data) => {
+    console.log(data)
     if (data?.code === 200) {
+      console.log(data, true)
        return localStorage.setItem('jwt-token', data?.data.tokens.access.token);
     }
   },
