28135575814bfa538c76b0069742142e25ddfdcb
Victor Ebube Okoroji <okorojiebube2@gmail.com>
2023-04-17 14:52:18 -0700

    Update index.tsx

--- src/routes/protected/admin/adminRoutes/index.tsx
+++ src/routes/protected/admin/adminRoutes/index.tsx
@@ -1,14 +1,14 @@
 
 import { Route, Routes } from 'react-router';
 import { adminRoutesData } from '../adminRoutesData';
-import { ProtectedRoute } from 'utils';
+import { AdminProtectedRoute } from 'utils';
 
 export const AdminRoutes = () => {
     const adminRoutes = adminRoutesData.map(({ path, id, element }) => {
         return <Route key={id} path={`/${path}`} element={
-            <ProtectedRoute>
+            <AdminProtectedRoute>
             {element}
-           </ProtectedRoute>
+           </AdminProtectedRoute>
         } />;
     });
 
