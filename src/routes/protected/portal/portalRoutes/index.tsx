349147d3c435ee147a1829e6cb62120481b9ce2f
Victor Ebube Okoroji <okorojiebube2@gmail.com>
2023-04-17 14:53:22 -0700

    Update index.tsx

--- src/routes/protected/portal/portalRoutes/index.tsx
+++ src/routes/protected/portal/portalRoutes/index.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router';
-import { ProtectedRoute } from 'utils';
+import { PortalProtectedRoute } from 'utils';
 import { portalRoutesData } from '../portalRoutesData';
 
 export const PortalRoutes = () => {
     const portalRoutes = portalRoutesData.map(({ path, id, element }) => {
         return <Route key={id} path={`/${path}`} element={
-          <ProtectedRoute>
+          <PortalProtectedRoute>
            {element}
-          </ProtectedRoute>
+          </PortalProtectedRoute>
         } />;
     });
     return <Routes>{portalRoutes}</Routes>;
