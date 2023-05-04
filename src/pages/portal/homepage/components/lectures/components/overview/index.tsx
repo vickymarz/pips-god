eaec0e350b74142e4ed0bdaa2de88d8df80c518c
vickymarz <okorojiebube2@gmail.com>
2023-05-04 16:53:29 +0100

    Fix portal styling

--- src/pages/portal/homepage/components/lectures/components/overview/index.tsx
+++ src/pages/portal/homepage/components/lectures/components/overview/index.tsx
@@ -16,26 +16,54 @@ export const LessonOverview = () => {
     setNumPages(nextNumPages);
   }
 
-  return (
-    <div>
-      <div className='flex justify-start items-start gap-x-[36px]'>
-        <h4 className="text-[2.5rem] font-bold font-productSans text-[#19275E]">{module?.title}</h4>
-      </div>
-      {module?.course_resources === undefined ||  module?.course_resources[1].url === '' ?
+  const resource = () => {
+      if(module?.code === 403) (
+        <div></div>
+      )
+
+      if((module?.course_resources === undefined ||  module?.course_resources[1].url === '') && module?.code !== 403) {
+        return (
         <div className='mb-[20px]'>
           <h2 className='text-[1.5rem] md:text-[2.5rem] my-4 font-medium font-productSans'>
             Welcome to pipsgod academy!
          </h2>
          <p className='md:text-[1.5rem]'>Kindly watch the video above to get yourself acquainted with the student learning portal.</p>
         </div>
-        :
-        <div>
+        )
+      } else if (module?.course_resources) {
+        return (
+          <div className='w-[80%]'>
           <Document file={module?.course_resources !== undefined ? module?.course_resources[1].url : ''} onLoadSuccess={onDocumentLoadSuccess}>
             {Array.from(new Array(numPages), (el, index) => (
               <Page key={`page_${index + 1}`} pageNumber={index + 1} />
             ))}
           </Document>
       </div>
+        )
+      }
+  }
+  return (
+    <div>
+      <div className='flex justify-start items-start gap-x-[36px]'>
+        <h4 className="text-[2.5rem] font-bold font-productSans text-[#19275E]">{module?.title}</h4>
+      </div>
+      {
+      // module?.course_resources === undefined ||  module?.course_resources[1].url === '' ?
+      //   <div className='mb-[20px]'>
+      //     <h2 className='text-[1.5rem] md:text-[2.5rem] my-4 font-medium font-productSans'>
+      //       Welcome to pipsgod academy!
+      //    </h2>
+      //    <p className='md:text-[1.5rem]'>Kindly watch the video above to get yourself acquainted with the student learning portal.</p>
+      //   </div>
+      //   :
+      //   <div>
+      //     <Document file={module?.course_resources !== undefined ? module?.course_resources[1].url : ''} onLoadSuccess={onDocumentLoadSuccess}>
+      //       {Array.from(new Array(numPages), (el, index) => (
+      //         <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+      //       ))}
+      //     </Document>
+      // </div>
+      resource()
       }
 
     </div>
