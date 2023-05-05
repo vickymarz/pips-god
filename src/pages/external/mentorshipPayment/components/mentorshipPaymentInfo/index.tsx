2255fffcbf8f9674d36300ab2c7a4ee1ef4ba0f9
vickymarz <okorojiebube2@gmail.com>
2023-05-05 18:54:07 +0100

    Modify signup form styling

--- src/pages/external/mentorshipPayment/components/mentorshipPaymentInfo/index.tsx
+++ src/pages/external/mentorshipPayment/components/mentorshipPaymentInfo/index.tsx
@@ -7,10 +7,10 @@ export const MentorshipPaymentInfo = () => {
     <div>
     <h2 className='pt-[14px] md:pt-[34px] pb-[21px] md:pb-[20px] text-[#19275E] text-[1.06rem] md:text-[2rem] font-semibold'>Mentorship payment | Pips god Academy</h2>
     <div className='flex flex-col justify-start items-start gap-y-[3px] md:gap-y-[6px]'>
-       <span className='text-[#8B8B8B] font medium text-[0.62rem] md:text-[1.2rem]'>USD</span>
+       <span className='text-[#8B8B8B] font medium text-[0.62rem] md:text-[1.2rem]'>USD | NAIRA</span>
        <h3 className='text-[#000000] font-medium text-[1.31rem] lg:text-[2rem]'>
-       <FontAwesomeIcon icon={faDollarSign} className='text-[1.2rem]'/>
-       500 (<FontAwesomeIcon icon={faNairaSign} className='text-[1.2rem]'/>350,000)</h3>
+       <FontAwesomeIcon icon={faDollarSign} className='text-[1.2rem] md:text-[1.8rem]'/>
+       500 (<FontAwesomeIcon icon={faNairaSign} className='text-[1.2rem] md:text-[1.8rem]'/>350,000)</h3>
     </div>
     <div className='font-medium text-[0.67rem] lg:text-[1.2rem] text-[#000000] flex flex-col justify-start items-start gap-y-[20px] md:gap-y-[40px] mb-[24px]'>
        <h4 className='text-[#19275E]'>Mentorship Details</h4>
--- src/pages/external/vipPayment/components/vipPaymentInfo/index.tsx
+++ src/pages/external/vipPayment/components/vipPaymentInfo/index.tsx
@@ -8,7 +8,7 @@ export const VipPaymentInfo = () => {
     <h2 className='pt-[14px] md:pt-[34px] pb-[21px] md:pb-[20px] text-[#19275E] text-[1.06rem] md:text-[2rem] font-semibold'>Vip Signal Group | Pips god Academy</h2>
     <div className='flex flex-col justify-start items-start gap-y-[3px] md:gap-y-[6px]'>
        <span className='text-[#8B8B8B] font medium text-[0.62rem] md:text-[1.2rem]'>USD | NAIRA</span>
-       <h3 className='text-[#000000] font-medium text-[1.31rem] lg:text-[2rem]'><FontAwesomeIcon icon={faDollarSign} className='text-[1.2rem]'/>100 (<FontAwesomeIcon icon={faNairaSign} className='text-[1.2rem]'/>50,000)</h3>
+       <h3 className='text-[#000000] font-medium text-[1.31rem] lg:text-[2rem]'><FontAwesomeIcon icon={faDollarSign} className='text-[1.2rem] md:text-[1.8rem]'/>100 (<FontAwesomeIcon icon={faNairaSign} className='text-[1.2rem] md:text-[1.8rem]'/>50,000)</h3>
     </div>
     <div className='font-medium text-[0.67rem] lg:text-[1.2rem] text-[#000000] flex flex-col justify-start items-start gap-y-[20px] md:gap-y-[40px] mb-[24px]'>
        <h4 className='text-[#19275E]'>Vip Signal Group Details</h4>
