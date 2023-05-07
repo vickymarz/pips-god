753323e788641c80a2c47839ff92a69cdb85d3e5
vickymarz <okorojiebube2@gmail.com>
2023-05-07 22:33:02 +0100

    Modify login styling

--- src/pages/external/adminSignin/index.tsx
+++ src/pages/external/adminSignin/index.tsx
@@ -4,7 +4,7 @@ import { AuthHeader } from "components";
 export const AdminSignIn = () => {
 	return (
 		<section className='w-full min-h-screen bg-authImg md:bg-none bg-cover bg-no-repeat'>
-			<div className="w-full min-h-screen flex items-center md:justify-start justify-center bg-[#ffffffe6] md:bg-[#F8FAFC]  px-[33px] md:px-[38px] pt-[45px] md:pt-[25px]">
+			<div className="w-full min-h-screen flex items-center md:justify-start justify-center bg-[#ffffffe6] md:bg-[#F8FAFC]  px-[33px] md:px-[38px] pt-[0] md:pt-[25px]">
 			<div className='flex flex-col justify-center items-center md:items-start gap-y-[1.25rem] md:gap-y-[4rem] md:w-full'>
 				<AuthHeader
 				title="Admin SignIn"
